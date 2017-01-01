552526ebbbd34b129bffc4deb1aeabfed1895977
yangfch3 <yangfch3@gmail.com>
2017-01-01 22:59:56 +0800

    去掉HTML模板注释，使之兼容低版本jQuery

--- src/template.js
+++ src/template.js
@@ -1,13 +1,8 @@
-export const TEMPLATE = `
-<div class="cropper-mask" id="cropperMask">
+export const TEMPLATE = `<div class="cropper-mask" id="cropperMask">
     <div class="cropper-box" id="cropperBox">
-        <!-- 图片 -->
-
-        <!-- cropper-box-mask 用于实现阴影遮罩 -->
         <div class="cropper-box-mask" id="cropperBoxMask"></div>
         <div class="cropper-box-client" id="cropperBoxClient">
             <div class="cropper-box-client-target" id="cropperBoxClientTarget">
-                <!-- 图片 -->
             </div>
             <div class="cropper-box-client-n cropper-box-client-bar"></div>
             <div class="cropper-box-client-s cropper-box-client-bar"></div>
